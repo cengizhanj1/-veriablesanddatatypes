0fa3cb5ea9b779698c36b03286cfe399a64a4b94
cengizhan alevok <alevokcengizhan@gmail.com>
2024-02-07 17:25:06 +0300

    veriablesanddatatypes

---
+++ veriablesanddatatypes.java
@@ -0,0 +1,165 @@
+package veriablesanddatatypes.java;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		
+		     //    --> single comment line
+		     /*       
+		    --> multiple comment lines
+		      */
+	//	Scanner scanner =new Scanner(System.in);//is used to receive an input from the keyboard.
+		
+		
+			//System.out.println("hello java");//first code
+			
+		
+		
+			// Introduce a value of type int  --> int number=1453;
+			/*
+			 
+			*** receiving and printing integer-type data ***
+			
+			String mesage="please give me any number:";
+			String mesage2="number is:";
+			System.out.println(mesage);
+			int number =scanner.nextInt();
+			System.out.println(mesage2+number);
+			*/
+			
+		
+		
+		//Introduce a value of type String mesage="1071 or alp";
+		    
+			/*
+			
+			***get and print string type values ***
+			
+			String mesage="please give me any string value:";
+			String mesage2="string value is:";
+			System.out.println(mesage);
+			String value =scanner.nextLine();
+			System.out.println(mesage2+value);
+			*/
+			
+		
+		
+		
+		//Introduce a value of type long number=1923
+		/*
+
+            ***get and print long type values***
+
+		String mesage="please give me any number:";
+		String mesage2="number is:";
+		System.out.println(mesage);
+		long  number =scanner.nextLong();
+		System.out.println(mesage2+number);
+		*/
+		
+		
+		
+		
+		//Introduce a value of type short number=2023;
+		
+		/*
+		 
+		***short type value retrieval and printing***
+		 
+		String mesage="please give me any number:";
+		String mesage2="number is:";
+		System.out.println(mesage);
+		short  number =scanner.nextShort();
+		System.out.println(mesage2+number);
+		
+		*/
+		
+		
+		
+		
+		//Introduce a value of type byte number=57;
+		/*
+		 
+		 ***get and print byte type values***
+		 
+		String mesage="please give me any number:";
+		String mesage2="number is:";
+		System.out.println(mesage);
+		byte  number =scanner.nextByte();
+		System.out.println(mesage2+number);
+		*/
+		
+		
+		
+		//Introduce a value of type double number=57.1;
+		/*
+		
+		***get and print double type values***
+		
+		String mesage="please give me any number:";
+		String mesage2="number is:";
+		System.out.println(mesage);
+		double  number =scanner.nextDouble();
+		System.out.println(mesage2+number);
+		*/
+		
+		
+		
+		//Introduce a value of type float number=67.43f;
+		/*
+		
+		***get and print float type values***
+		
+		String mesage="please give me any number:";
+		String mesage2="number is:";
+		System.out.println(mesage);
+		float  number =scanner.nextFloat();
+		System.out.println(mesage2+number);
+		*/
+		
+		
+		
+		//Introduce a value of type char x='w';or char y='7';
+		/*
+		
+		***get and print char type values***
+		
+		String mesage="please give me any char:";
+		String mesage2="char is:";
+		System.out.println(mesage);
+		char charr =scanner.next().charAt(0);
+		System.out.println(mesage2+charr);
+		*/
+		
+		
+		
+		
+		//Introduce a value of type boolean c=true; or boolean a=false;
+		/*
+		
+		 ***get and print boolean type values***
+		
+		String mesage="please give me boolean valeu(true or false):";
+		String mesage2="boolean  is:";
+		System.out.println(mesage);
+		boolean x =scanner.nextBoolean();
+		System.out.println(mesage2+x);
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+}
